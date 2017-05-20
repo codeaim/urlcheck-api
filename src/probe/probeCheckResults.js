637c5c674b994d942fcf6214dc4581abe9163b98
gdownes <gdownes@gmail.com>
2017-05-20 23:55:57 +0100

    Rework SQL to update correct rows

--- src/probe/probeCheckResults.js
+++ src/probe/probeCheckResults.js
@@ -18,30 +18,32 @@ module.exports.handler = (event, context, callback) => {
     client.tx(t => {
         const responseInsertBatchSql = `
             INSERT INTO response (check_id, duration, region, created)
-            VALUES ${checkResults.map((checkResult) => `('${checkResult.id}', ${checkResult.responseTime}, '${checkResult.region}', now());`).join(', ')}`;
+            VALUES ${checkResults.map((checkResult) => `('${checkResult.id}', ${checkResult.responseTime}, '${checkResult.region}', now())`).join(', ')};`;
 
         const changeInsertBatchSql = `
             INSERT INTO change (check_id, status, status_code, created)
-            VALUES ${checkResults.filter((checkResult) => checkResult.status !== checkResult.previousStatus && checkResult.confirming).map((checkResult) => `('${checkResult.id}', '${checkResult.status}', ${checkResult.statusCode}, now());`).join(', ')}`;
+            VALUES ${checkResults.filter((checkResult) => checkResult.status !== checkResult.previousStatus && checkResult.confirming).map((checkResult) => `('${checkResult.id}', '${checkResult.status}', ${checkResult.statusCode}, now())`).join(', ')};`;
 
         const checkUpdateSql = `
             UPDATE "check"
             SET
-                status = CASE WHEN c.id IN ('${confirmed.join('\', \'')}') THEN change.status ELSE c.status END,
+                status = CASE WHEN "check".id IN ('${confirmed.join('\', \'')}') THEN change.status ELSE "check".status END,
                 state = 'WAITING',
                 modified = NOW(),
-                refresh = CASE WHEN c.id IN ('${unconfirmed.join('\', \'')}') THEN NOW() ELSE (NOW() + (c.interval * interval '1 minute')) END,
+                refresh = CASE WHEN "check".id IN ('${unconfirmed.join('\', \'')}') THEN NOW() ELSE (NOW() + ("check".interval * interval '1 minute')) END,
                 locked = NULL,
-                version = c.version + 1,
-                confirming = c.id IN ('${unconfirmed.join('\', \'')}'),
+                version = "check".version + 1,
+                confirming = "check".id IN ('${unconfirmed.join('\', \'')}'),
                 region = response.region,
                 disabled = CASE WHEN "user".credit <= 1 THEN NOW() ELSE NULL END
             FROM "check" c
             INNER JOIN "user" ON c.username = "user".username
             LEFT JOIN change ON c.id = change.check_id
             INNER JOIN response ON c.id = response.check_id
-            WHERE response.created = (SELECT max(response.created) FROM response WHERE response.check_id = c.id)
-            AND (change.created = (SELECT max(change.created) FROM change WHERE change.check_id = c.id) OR change.created IS NULL);`;
+            WHERE "check".id IN ('${checkResults.map((checkResult) => checkResult.id).join('\', \'')}')
+            AND "check".id = c.id
+            AND response.created = (SELECT max(response.created) FROM response WHERE response.check_id = "check".id)
+            AND (change.created = (SELECT max(change.created) FROM change WHERE change.check_id = "check".id) OR change.created IS NULL);`;
 
         console.log(`Insert responses sql: ${responseInsertBatchSql}`);
         if(confirmed.length > 0)  console.log(`Insert changes sql: ${changeInsertBatchSql}`);
