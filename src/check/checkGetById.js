47479bd6b6cbdc2814b8df93b65ecaeccc70b68c
gdownes <gdownes@gmail.com>
2017-05-22 00:48:40 +0100

    Adding getUserByUsername & getCheckById endpoint implementation

--- src/check/checkGetById.js
+++ src/check/checkGetById.js
@@ -1,17 +1,124 @@
 'use strict';
 
-const pg = require('pg');
+const pgp = require('pg-promise')();
+const client = pgp(process.env.DATABASE_URL);
 
 module.exports.handler = (event, context, callback) => {
-    const checkId = JSON.parse(event.body)['check-id'];
-    console.log(checkId);
-    const response = {
-        statusCode: 200,
-        headers: {},
-        body: JSON.stringify({
-            id: "hello"
-        })
-    };
+    const checkId = event.pathParameters['check-id'];
+    const includeChanges = event.queryStringParameters !== null
+        ? event.queryStringParameters.includeChanges
+        : false;
+    const includeResponses = event.queryStringParameters !== null
+        ? event.queryStringParameters.includeResponses
+        : false;
 
-    callback(null, response);
+    client.task((task) => task.batch(
+        includeChanges && includeResponses
+            ? [task.oneOrNone(getCheckSql, {checkId}), task.manyOrNone(getChangesSql, {checkId}), task.manyOrNone(getResponsesSql, {checkId})]
+            : includeChanges
+                ? [task.oneOrNone(getCheckSql, {checkId}), task.manyOrNone(getChangesSql, {checkId})]
+                : includeResponses
+                    ? [task.oneOrNone(getCheckSql, {checkId}), task.manyOrNone(getResponsesSql, {checkId})]
+                    : [task.oneOrNone(getCheckSql, {checkId})]
+    )).then((events) => {
+        const check = events[0] !== null
+            ? Object.assign(
+                getCheck(events[0]),
+                includeChanges
+                    ? getChanges(events[1])
+                    : {},
+                includeResponses
+                    ? getResponses(includeChanges ? events[2] : events[1])
+                    : {}
+              )
+            : null;
+
+        const response = createResponse(check);
+
+        pgp.end();
+        callback(null, response);
+    }).catch(error => {
+        pgp.end();
+        callback(error);
+    });
 };
+
+const createResponse = c => {
+    return {
+        statusCode: c !== null ? 200 : 404,
+        headers: {},
+        body: c !== null
+            ? JSON.stringify(c)
+            : ""
+    };
+};
+
+const getCheck = c => {
+    return {
+        id: c.id,
+        name: c.name,
+        url: c.url,
+        protocol: c.protocol,
+        interval: c.interval,
+        status: c.status,
+        disabled: c.disabled,
+        headers: c.headers
+    };
+};
+
+const getChanges = ch => {
+    return {
+        changes: ch.map((c) => getChange(c))
+    };
+};
+
+const getChange = c => {
+    return {
+        status: c.status,
+        statusCode: c.status_code,
+        credit: c.created
+    };
+};
+
+const getResponses = re => {
+    return {
+        responses: re.map((r) => getResponse(r))
+    };
+};
+
+const getResponse = r => {
+    return {
+        duration: r.duration,
+        region: r.region,
+        created: r.created
+    };
+};
+
+const getCheckSql = `
+    SELECT
+        id,
+        name,
+        url,
+        protocol,
+        interval,
+        status,
+        disabled,
+        headers
+    FROM "check"
+    WHERE id = $[checkId]`;
+
+const getChangesSql = `
+    SELECT
+        status,
+        status_code,
+        created
+    FROM change
+    WHERE check_id = $[checkId]`;
+
+const getResponsesSql = `
+    SELECT
+        duration,
+        region,
+        created
+    FROM response
+    WHERE check_id = $[checkId]`;
--- src/user/userGetByUsername.js
+++ src/user/userGetByUsername.js
@@ -1,17 +1,92 @@
 'use strict';
 
-const pg = require('pg');
-const AWS = require("aws-sdk");
+const pgp = require('pg-promise')();
+const client = pgp(process.env.DATABASE_URL);
 
 module.exports.handler = (event, context, callback) => {
+    const username = event.pathParameters.username;
+    const includeChecks = event.queryStringParameters !== null
+        ? event.queryStringParameters.includeChecks
+        : false;
 
-        const response = {
-            statusCode: 200,
-            headers: {},
-            body: JSON.stringify({
-                id: "hello"
-            })
-        };
+    client.task((task) => task.batch(
+        includeChecks
+            ? [task.oneOrNone(getUserSql, {username}), task.manyOrNone(getChecksSql, {username})]
+            : [task.oneOrNone(getUserSql, {username})]
+    )).then((events) => {
+        const user = events[0] !== null
+            ? Object.assign(
+                getUser(events[0]),
+                includeChecks && events[1] !== null
+                    ? getChecks(events[1])
+                    : {}
+              )
+            : null;
 
+        const response = createResponse(user);
+
+        pgp.end();
         callback(null, response);
+    }).catch(error => {
+        pgp.end();
+        callback(error);
+    });
 };
+
+const createResponse = u => {
+    return {
+        statusCode: u !== null ? 200 : 404,
+        headers: {},
+        body: u !== null
+            ? JSON.stringify(u)
+            : ""
+    };
+};
+
+const getUser = u => {
+    return {
+        username: u.username,
+        admin: u.admin,
+        credit: u.credit
+    };
+};
+
+const getCheck = c => {
+    return {
+        id: c.id,
+        name: c.name,
+        url: c.url,
+        protocol: c.protocol,
+        interval: c.interval,
+        status: c.status,
+        disabled: c.disabled,
+        headers: c.headers
+    };
+};
+
+const getChecks = ch => {
+    return {
+        checks: ch.map((c) => getCheck(c))
+    };
+};
+
+const getChecksSql = `
+    SELECT
+        id,
+        name,
+        url,
+        protocol,
+        interval,
+        status,
+        disabled,
+        headers
+    FROM "check"
+    WHERE username = $[username]`;
+
+const getUserSql = `
+    SELECT
+        username,
+        admin,
+        credit
+    FROM "user"
+    WHERE username = $[username]`;
